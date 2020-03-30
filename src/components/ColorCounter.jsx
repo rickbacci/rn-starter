8452828d917c4500d8830979dfceb29c38268f88
rickbacci <rickbacci@gmail.com>
2020-03-30 14:20:58 -0600

    update styles

--- src/components/ColorCounter.jsx
+++ src/components/ColorCounter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   View,
   Text,
@@ -6,16 +7,43 @@ import {
   StyleSheet,
 } from 'react-native';
 
-const ColorCounter = ({ color, onIncrease, onDecrease }) => {
-  return (
-    <View>
-      <Text>{color}</Text>
-      <Button onPress={() => onIncrease()} title={`Increase ${color}`} />
-      <Button onPress={() => onDecrease()} title={`Decrease ${color}`} />
-    </View>
-  );
-};
+const styles = StyleSheet.create({
+  textStyle: {
+    margin: 5,
+    textAlign: 'center',
+  },
+  viewStyle: {
+    margin: 2,
+    textAlign: 'center',
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+  buttonStyle: {
+    padding: 5,
+    margin: 5,
+  },
+});
 
-const styles = StyleSheet.create({});
+const ColorCounter = ({ color, onIncrease, onDecrease }) => (
+  <View style={styles.viewStyle}>
+    <Text style={styles.textStyle}>{color}</Text>
+    <Button
+      style={styles.buttonStyle}
+      onPress={() => onIncrease()}
+      title={`Increase ${color}`}
+    />
+    <Button
+      style={styles.buttonStyle}
+      onPress={() => onDecrease()}
+      title={`Decrease ${color}`}
+    />
+  </View>
+);
+
+ColorCounter.propTypes = {
+  color: PropTypes.string.isRequired,
+  onIncrease: PropTypes.func.isRequired,
+  onDecrease: PropTypes.func.isRequired,
+};
 
 export default ColorCounter;
