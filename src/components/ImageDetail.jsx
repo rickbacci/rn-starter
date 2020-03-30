62ad07690bf903c6fc2ade458a50c708269288f1
rickbacci <rickbacci@gmail.com>
2020-03-30 14:34:28 -0600

    add some style to ImageDetail

--- src/components/ImageDetail.jsx
+++ src/components/ImageDetail.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   View,
   Text,
@@ -6,19 +7,37 @@ import {
   Image,
 } from 'react-native';
 
-const ImageDetail = ({ imageSource, title, imageScore }) => {
-  return (
-    <View>
-      <Image source={imageSource} />
-      <Text>{title}</Text>
-      <Text>
-        Image score -
-        {imageScore}
-      </Text>
-    </View>
-  );
-};
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderColor: 'black',
+    borderWidth: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    margin: 5,
+    padding: 5,
+  },
+  imageStyle: {
+    borderColor: 'grey',
+    borderWidth: 5,
+    borderRadius: 15,
+  },
+});
 
-const styles = StyleSheet.create({});
+const ImageDetail = ({ imageSource, title, imageScore }) => (
+  <View style={styles.viewStyle}>
+    <Text>{title}</Text>
+    <Image style={styles.imageStyle} source={imageSource} />
+    <Text>
+      Image score -
+      {imageScore}
+    </Text>
+  </View>
+);
+
+ImageDetail.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageScore: PropTypes.number.isRequired,
+  imageSource: PropTypes.string.isRequired,
+};
 
 export default ImageDetail;
