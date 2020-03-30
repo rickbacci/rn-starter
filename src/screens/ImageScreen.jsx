bce8f3427715666bd8d894042e14c5bd491cfd3d
rickbacci <rickbacci@gmail.com>
2020-03-30 15:14:42 -0600

    add styles to ImageScreen

--- src/screens/ImageScreen.jsx
+++ src/screens/ImageScreen.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import ImageDetail from '../components/ImageDetail';
 
 const forest = require('../../assets/forest.jpg');
 const beach = require('../../assets/beach.jpg');
 const mountain = require('../../assets/mountain.jpg');
 
-const ImageScreen = () => {
-  return (
-    <View>
-      <ImageDetail
-        title="Forest"
-        imageSource={forest}
-        imageScore={9}
-      />
-      <ImageDetail
-        title="Beach"
-        imageSource={beach}
-        imageScore={8}
-      />
-      <ImageDetail
-        title="Mountain"
-        imageSource={mountain}
-        imageScore={7}
-      />
-    </View>
-  );
-};
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+    alignSelf: 'center',
+  },
+});
 
-const styles = StyleSheet.create({});
+const ImageScreen = () => (
+  <View>
+    <Text style={styles.textStyle}>Images</Text>
+    <ImageDetail
+      title="Forest"
+      imageSource={forest}
+      imageScore={9}
+    />
+    <ImageDetail
+      title="Beach"
+      imageSource={beach}
+      imageScore={8}
+    />
+    <ImageDetail
+      title="Mountain"
+      imageSource={mountain}
+      imageScore={7}
+    />
+  </View>
+);
 
 export default ImageScreen;
