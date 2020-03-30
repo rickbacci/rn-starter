c3b9bb03d9f9861b3c65b0c2c040ce723f9e87ca
rickbacci <rickbacci@gmail.com>
2020-03-30 15:11:35 -0600

    add styles to home screen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,57 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Text,
   StyleSheet,
   View,
   Button,
 } from 'react-native';
-import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
-  text: {
+  textStyle: {
     fontSize: 30,
+    alignSelf: 'center',
+    margin: 10,
   },
 });
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text style={styles.text}>HomeScreen</Text>
-      <Button
-        title="Go to Components Demo"
-        onPress={() => navigation.navigate('Components')}
-      />
-      <Button
-        title="Go to List Demo"
-        onPress={() => navigation.navigate('List')}
-      />
-      <Button
-        title="Go to Images Demo"
-        onPress={() => navigation.navigate('Images')}
-      />
-      <Button
-        title="Go to Counter Demo"
-        onPress={() => navigation.navigate('Counter')}
-      />
-      <Button
-        title="Go to Colors Demo"
-        onPress={() => navigation.navigate('Colors')}
-      />
-      <Button
-        title="Go to Square Demo"
-        onPress={() => navigation.navigate('Square')}
-      />
-      <Button
-        title="Go to Text Demo"
-        onPress={() => navigation.navigate('Text')}
-      />
-      <Button
-        title="Go to Box Demo"
-        onPress={() => navigation.navigate('Box')}
-      />
-    </View>
-  );
-};
+const HomeScreen = ({ navigation }) => (
+  <View>
+    <Text style={styles.textStyle}>HomeScreen</Text>
+    <Button
+      title="Go to Components Demo"
+      onPress={() => navigation.navigate('Components')}
+    />
+    <Button
+      title="Go to List Demo"
+      onPress={() => navigation.navigate('List')}
+    />
+    <Button
+      title="Go to Images Demo"
+      onPress={() => navigation.navigate('Images')}
+    />
+    <Button
+      title="Go to Counter Demo"
+      onPress={() => navigation.navigate('Counter')}
+    />
+    <Button
+      title="Go to Colors Demo"
+      onPress={() => navigation.navigate('Colors')}
+    />
+    <Button
+      title="Go to Square Demo"
+      onPress={() => navigation.navigate('Square')}
+    />
+    <Button
+      title="Go to Text Demo"
+      onPress={() => navigation.navigate('Text')}
+    />
+    <Button
+      title="Go to Box Demo"
+      onPress={() => navigation.navigate('Box')}
+    />
+  </View>
+);
 
 HomeScreen.propTypes = {
   navigation: PropTypes.shape({
