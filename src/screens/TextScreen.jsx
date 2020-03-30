298640799329988e3c794760896e061b9b3c8f0a
rickbacci <rickbacci@gmail.com>
2020-03-30 11:40:13 -0600

    add text demos

---
+++ src/screens/TextScreen.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
+
+const TextScreen = () => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const invalidPassword = () => {
+    if (password.length < 5) {
+      return true;
+    }
+    return false;
+  };
+
+  const showWarning = () => (
+    <Text>
+      Password must be at least 5 characters long!
+    </Text>
+  );
+
+
+  return (
+    <View>
+      <Text>
+        Enter Name:
+      </Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      <Text>
+        Hello my name is:
+        {name}
+      </Text>
+      <Text>
+        Enter Password:
+      </Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
+      />
+      {invalidPassword() ? showWarning() : null}
+    </View>
+  );
+};
+
+export default TextScreen;
